93d750d86aacd2b5bd1ff1fa5fc11c29ad1334bc
cort-robinson <cortrobinson99@gmail.com>
2022-03-20 23:49:53 -0500

    API now works with query string phoneNumber param on /number route

--- lambda/.aws-sam/build/HelloWorldFunction/app.js
+++ lambda/.aws-sam/build/HelloWorldFunction/app.js
@@ -1,6 +1,9 @@
 // const axios = require('axios')
 // const url = 'http://checkip.amazonaws.com/';
+const AWS = require('aws-sdk');
 const isWord = require('is-word');
+
+const docClient = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' });
 let response;
 
 /**
@@ -17,15 +20,19 @@ let response;
  */
 exports.lambdaHandler = async (event, context) => {
     try {
-        // const ret = await axios(url);
+        console.log('Processing...');
 
+        // letterMap for letterCombos function
         const letterMap = { '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz' };
 
         // gets all the possible letter combinations of a given number
-        // currently does not support numbers with 1's or 0's since they are not included in the letterMap
+        // 1's and 0's in numbers trigger function to return empty string
         const letterCombos = function (number) {
-            let len = number.length, ans = [];
+            // if (number === null) return [];
+            if (number.includes('0') || number.includes('1')) return [];
+            const len = number.length;
             if (!len) return [];
+            let ans = [];
 
             // depth first search
             const dfs = (pos, str) => {
@@ -41,38 +48,62 @@ exports.lambdaHandler = async (event, context) => {
             return ans;
         };
 
-        // phoneNumber = event.pathParameters.phoneNumber;
-        phoneNumber = '1-800-356-9377';
-        // remove all non-numeric characters
-        reducedNumber = phoneNumber.replace(/\D/g, '');
+        // initialize phoneNumber as null, then set it to the event.queryStringParameters.phoneNumber if valid
+        let phoneNumber = null;
+        let validWords = null;
 
-        wordsList = [];
+        if (event.queryStringParameters && event.queryStringParameters.phoneNumber) {
+            phoneNumber = event.queryStringParameters.phoneNumber;
 
-        wordsList.push(...letterCombos(reducedNumber.slice(-7)));
-        wordsList.push(...letterCombos(reducedNumber.slice(-6)));
-        wordsList.push(...letterCombos(reducedNumber.slice(-5)));
-        wordsList.push(...letterCombos(reducedNumber.slice(-4)));
-        wordsList.push(...letterCombos(reducedNumber.slice(-3)));
+            // remove all non-numeric characters from phoneNumber
+            const reducedNumber = phoneNumber.replace(/\D/g, '');
 
-        // const isWord = require('is-word');
+            // initialize wordsList to store valid vanity number words
+            let wordsList = [];
+            // push possible vanity words to wordsList in order of highgest to lowest length
+            wordsList.push(...letterCombos(reducedNumber.slice(-7)));
+            wordsList.push(...letterCombos(reducedNumber.slice(-6)));
+            wordsList.push(...letterCombos(reducedNumber.slice(-5)));
+            wordsList.push(...letterCombos(reducedNumber.slice(-4)));
+            wordsList.push(...letterCombos(reducedNumber.slice(-3)));
 
-        // initialize is-word import
-        const englishWords = isWord('american-english');
+            // initialize is-word package to check if word is valid
+            const englishWords = isWord('american-english');
 
-        // filter out letter combinations that are not english words and get the first 5
-        const validWords = wordsList.filter(word => englishWords.check(word));
-        if (validWords.length > 5) validWords.splice(5);
+            // filter out letter combinations that are not english words and get the first 5
+            validWords = wordsList.filter(word => englishWords.check(word));
+            if (validWords.length > 5) validWords.splice(5);
+        }
 
-        // for testing purposes
-        // console.log(validWords);
+        // parameters for dynamoDB query
+        const params = {
+            Item: {
+                date: Date.now(),
+                message: validWords
+            },
+            TableName: 'vanity-numbers'
+        };
 
+        // http response
         response = {
             'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true
+            },
             'body': JSON.stringify({
-                message: validWords,
-                // location: ret.data.trim()
+                message: validWords && validWords.length > 0 ? validWords : 'no valid words found',
             })
         };
+
+        // insert into dynamoDB
+        docClient.put(params, function (err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('db write success');
+            }
+        });
     } catch (err) {
         console.log(err);
         return err;
